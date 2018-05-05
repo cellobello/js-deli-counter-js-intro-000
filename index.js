2f5fcab04c2dc8b8cd625f3e70d656aeea915e79
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-05 16:22:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeli) {
   if(katzDeli.length === 0) {
     return "The line is currently empty.";
   } else {
-  lineArray = ["The line is currently: "];
+  var lineArray = ["The line is currently: "];
 	for (var i = 0; i < (katzDeli.length -1); i++) {
   		lineArray.push( (i + 1) + ". " + katzDeli[i] + ", " );
 		}
